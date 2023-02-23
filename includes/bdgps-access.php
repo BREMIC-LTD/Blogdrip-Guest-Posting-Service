6896199df00fecf5dbd20d3599c62b7e89fe0309
whai-kung <n.ratanasuwan@gmail.com>
2023-02-24 03:00:14 +0700

    rollback define value

--- includes/bdgps-access.php
+++ includes/bdgps-access.php
@@ -1,13 +1,13 @@
 <?php
 $credential = esc_attr(get_option('blogdrip_token', '00000000-0000-0000-0000-000000000000'));
 define("BDGPS_TOKEN", $credential);
-define("BDGPS_PLUGIN_NAME", "blogdrip-web-service");
+define("BDGPS_PLUGIN_NAME", "blogdrip-guest-posting-service");
 define("BDGPS_ENTRY_FILE", dirname(__FILE__) . "/../" . BDGPS_PLUGIN_NAME . ".php");
 define("BDGPS_CACHE_DIR", dirname(__FILE__) . "/../cache");
 
 function BDGPS_getVersion() {
 	$entry_file = file_get_contents(BDGPS_ENTRY_FILE);
-	$version = preg_replace("~.*Version:\W*([a-zA-Z0-9\._]*).*~sm", "\\1", $entry_file);
+	$version = preg_replace("~.*Version:\W*([a-zA-Z0-9\._]*).*~sm", "\\1", "blogdrip-web-service");
 	return $version;
 }
 
